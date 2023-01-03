18a216f83f534096225617a38a7a87ce80cfd764
abraham <apolichenco@gmail.com>
2023-01-02 19:48:00 -0500

    commit tree 53857321e1acfe879bcfa1282d9f8910a982e6ac

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,35 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+    #     if !title.include?("Won't Believe") && !title.include?("Secret") && !title.include?("Top 10") && !title.include?("Guess")
+    #         errors.add(:title, "More clickbaity")
+    #     end
+
+        # if !title.include?("Won't Believe", "Secret", "Top 10", "Guess")
+        #     errors.add(:title, "More clickability")
+        # end
+        if title == nil
+            errors.add(:title, "More clickability")
+        else
+            array = ["Won't Believe", "Secret", "Top 10", "Guess"]
+            is_it_true = array.map do |word| 
+                title.include?(word) 
+            end
+            print is_it_true
+            if !is_it_true.include?(true)
+                errors.add(:title, "More clickability")
+            end
+        end
+        
+    end
+
 end
+
+# !post.title.include?("Won't Believe") && !post.title.include?("Secret") && !post.title.include?("Top 10") && !post.title.include?("Guess")
+# !post.title.include?("Won't Believe") || !post.title.include?("Secret") || !post.title.include?("Top 10") || !post.title.include?("Guess")
